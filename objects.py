69480edb31c37168cf339c174cbf8e12fc05f0d2
fogjama <66557609+fogjama@users.noreply.github.com>
2020-09-27 13:53:15 +0100

    Define init classes and basic functions

--- objects.py
+++ objects.py
@@ -1,25 +1,54 @@
 import pygame
 from pygame.locals import *
+import os
 
 ALPHA = (255,255,255)
 
 class Player(pygame.sprite.Sprite):
 
-    # @TODO: Spawn player
-    def __init__(self):
-        pass
+    # Spawn player
+    def __init__(self, spawnx, spawny, health, maxhealth, img, img_frames, ALPHA=ALPHA):
+        pygame.sprite.Sprite.__init__(self)
+        self.health = health
+        self.maxhealth = maxhealth
+        self.frame = 0              # count frames
+        self.movex = 0              # move along x
+        self.movey = 0              # move along y
+        self.images = []
+        self.is_jumping = False     
+        self.is_falling = True
 
-    # @TODO: Control player movement
-    def move(self):
-        pass
+        for i in range(0,img_frames):
+            image = pygame.image.load(os.path.join('assets/', f'{img}{i}.png')).convert()
+            image.convert_alpha()
+            image.set_colorkey(ALPHA)
+            self.images.append(image)
+        
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.rect.x = spawnx
+        self.rect.y = spawny
 
-    # @TODO: Define player gravity
-    def gravity(self):
-        pass
+    # Control player movement
+    def move(self, x, y):
+        self.movex += x
+        self.movey += y
 
-    # @TODO: Define player jump
+    # Define player gravity
+    def gravity(self, fall_speed):
+        if self.is_falling:
+            self.movey += fall_speed
+
+    # Define player jump
     def jump(self):
-        pass
+        if self.is_jumping is False:
+            self.is_falling = False
+            self.is_jumping = True
+    
+    def stop_jumping(self):
+        if self.is_jumping:
+            self.is_falling = True
+            self.is_jumping = False
 
     # @TODO: Update player 
     def update(self):
@@ -29,15 +58,37 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
 
     # @TODO: Spawn enemy
-    def __init__(self):
-        pass
+    def __init__(self, spawnx, spawny, img, img_frames, type, ALPHA=ALPHA):
+        pygame.sprite.Sprite.__init__(self)
+        self.frame = 0
+        self.images = []
+        self.type = type
+        
+        for i in range(0,img_frames):
+            image = pygame.image.load(os.path.join('assets/', f'{img}{i}.png')).convert()
+            image.convert_alpha()
+            image.set_colorkey(ALPHA)
+            self.images.append(image)
+        
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.rect.x = spawnx
+        self.rect.y = spawny
+        self.counter = 0
 
     # @TODO: Set enemy movement
     def move(self):
         pass
 
+    def gravity(self, fall_speed):
+        pass
+
 
 class EnemyFlying(Enemy):
+
+    # @TODO: Overwrite gravity function
+    def gravity(self):
+        pass
 
     # @TODO: Set enemy movement
     def fly(self):
@@ -50,8 +101,14 @@ class EnemyFlying(Enemy):
 class Platform(pygame.sprite.Sprite):
     
     # @TODO: Spawn platform
-    def __init__(self):
-        pass
+    def __init__(self, xloc, yloc, imgw, imgh, img, ALPHA=ALPHA):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('assets/', f'{img}.png')).convert()
+        self.image.convert_alpha()
+        self.image.set_colorkey(ALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.x = xloc
+        self.rect.y = yloc
 
 
 class Level():
@@ -65,7 +122,7 @@ class Level():
 
     # @TODO: Initialise enemy list
     def enemies(self):
-        
+
         pass
 
     # @TODO: Initialise platform list
