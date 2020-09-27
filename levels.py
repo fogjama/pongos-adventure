213c41b48ed3534082301482832be9efd4f87930
fogjama <66557609+fogjama@users.noreply.github.com>
2020-09-27 20:42:56 +0100

    Create level selection template

--- levels.py
+++ levels.py
@@ -1,47 +1,18 @@
 import pygame
-from objects import Platform
+from objects import Level
 
 
-def generate_simple_ground(tilex, tiley, worldx, worldy, img, ground_height=0):
+def select_level(lvl):
+    switcher = {
+        1: level_1,
+        2: level_2
+    }
 
-    # If ground height is not specified, set to tile height
-    if ground_height == 0:
-        ground_height = worldy - tiley
-    
-    gloc = []
-    gloc.append({
-        'x': 0,
-        'y': ground_height,
-        'w': tilex,
-        'length': (worldx/tilex)+tilex,
-        'img': img
-    })
+    lvl_func = switcher.get(lvl, lambda: print('Level does not exist'))
+    lvl_func()
 
-    ground_list = generate_platforms(gloc)
+def level_1():
+    print('Level 1')
 
-    return ground_list
-
-
-def generate_platforms(ploc):
-    # ploc in format {x:0, y:0, w:0, img:0, length:0, optional ALPHA:ALPHA}
-    plat_list = pygame.sprite.Group()
-
-    # Check for presence of ALPHA
-    if len(ploc) > 0:
-        if len(ploc[0]) == 6:
-            for p in ploc:
-                if 'ALPHA' in p.keys():
-                    i = 0
-                    while i <= p['length']:
-                        plat = Platform(p['x']+(i*p['w']), p['y'], p['img'], p['ALPHA'])
-                        plat_list.add(plat)
-                else:
-                    i = 0
-                    while i <= p['length']:
-                        plat = Platform(p['x']+(i*p['w']), p['y'], p['img'])
-                        plat_list.add(plat)
-    
-    return plat_list
-    
-
-
+def level_2():
+    print('Level 2')
