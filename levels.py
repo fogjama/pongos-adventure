5449691d9a0beced44ee54d387a03a77a7bf7532
fogjama <66557609+fogjama@users.noreply.github.com>
2020-09-27 22:10:11 +0100

    Create initial values for levels 1 and 2

--- levels.py
+++ levels.py
@@ -1,18 +1,58 @@
-import pygame
-from objects import Level
 
-
-def select_level(lvl):
+def select_level(lvl, worldx, worldy):
     switcher = {
         1: level_1,
         2: level_2
     }
 
-    lvl_func = switcher.get(lvl, lambda: print('Level does not exist'))
-    lvl_func()
+    lvl_func = switcher.get(lvl, lambda: level_1)
+    return lvl_func(worldx, worldy)
 
-def level_1():
-    print('Level 1')
+def level_1(worldx, worldy):
+    background = 'stage'
+    gloc = {
+        'tilex': 0,
+        'tiley': 0,
+        'worldx': worldx,
+        'worldy': worldy,
+        'img': '',
+        'ground_height': 0
+    }
 
-def level_2():
-    print('Level 2')
+    ploc = []
+    ploc.append({
+        'x':0,
+        'y':0,
+        'w':0,
+        'length':0,
+        'img':'',
+        'ALPHA':(0,255,0)
+    })
+
+    return {'ploc': ploc,'gloc': gloc, 'background': background}
+
+def level_2(worldx, worldy):
+    background = 'stage1'
+    gloc = {
+        'tilex': 0,
+        'tiley': 0,
+        'worldx': worldx,
+        'worldy': worldy,
+        'img': '',
+        'ground_height': 0
+    }
+
+    ploc = []
+    ploc.append({
+        'x':0,
+        'y':0,
+        'w':0,
+        'length':0,
+        'img':'',
+        'ALPHA':(0,255,0)
+    })
+
+    return {'ploc': ploc,'gloc': gloc, 'background': background}
+
+if __name__ == "__main__":
+    print(select_level(1, 800, 600))
